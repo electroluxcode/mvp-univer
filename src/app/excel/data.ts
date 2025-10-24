986142dce7f77397fe2f86a00f9a3d0c949d289b
electroluxcode <3451613934@qq.com>
2025-10-24 18:10:02 +0800

    update base

---
+++ src/app/excel/data.ts
@@ -0,0 +1,160 @@
+import { IWorkbookData } from "@univerjs/core";
+
+// 默认示例数据
+export const DEFAULT_DATA: Partial<IWorkbookData> = {
+    "id": "workbook-01",
+    "sheetOrder": [
+      "sheet-01",
+      "nEMAIabptfMALhBKGrkkV"
+    ],
+    "name": "My Workbook",
+    "appVersion": "0.10.12",
+    "styles": {},
+    "sheets": {
+      "sheet-01": {
+        "id": "sheet-01",
+        "name": "Sheet1",
+        "cellData": {
+          "0": {
+            "0": {
+              "v": "Name adsa2323",
+              "t": 1
+            },
+            "1": {
+              "v": "Age"
+            },
+            "2": {
+              "v": "City"
+            }
+          },
+          "1": {
+            "0": {
+              "v": "Alice"
+            },
+            "1": {
+              "v": 25
+            },
+            "2": {
+              "v": "Beijing"
+            }
+          },
+          "2": {
+            "0": {
+              "v": "Bob"
+            },
+            "1": {
+              "v": 30
+            },
+            "2": {
+              "v": "Shanghai"
+            }
+          }
+        },
+        "tabColor": "",
+        "hidden": 0,
+        "rowCount": 1000,
+        "columnCount": 20,
+        "zoomRatio": 1,
+        "freeze": {
+          "xSplit": 0,
+          "ySplit": 0,
+          "startRow": -1,
+          "startColumn": -1
+        },
+        "scrollTop": 0,
+        "scrollLeft": 0,
+        "defaultColumnWidth": 88,
+        "defaultRowHeight": 24,
+        "mergeData": [],
+        "rowData": {},
+        "columnData": {
+          "0": {
+            "w": 260.60546875
+          }
+        },
+        "showGridlines": 1,
+        "rowHeader": {
+          "width": 46,
+          "hidden": 0
+        },
+        "columnHeader": {
+          "height": 20,
+          "hidden": 0
+        },
+        "rightToLeft": 0
+      },
+      "nEMAIabptfMALhBKGrkkV": {
+        "id": "nEMAIabptfMALhBKGrkkV",
+        "name": "Sheet1 （副本）",
+        "cellData": {
+          "0": {
+            "0": {
+              "v": "Name adsa2323232323",
+              "t": 1
+            },
+            "1": {
+              "v": "Age"
+            },
+            "2": {
+              "v": "City"
+            }
+          },
+          "1": {
+            "0": {
+              "v": "Alice"
+            },
+            "1": {
+              "v": 25
+            },
+            "2": {
+              "v": "Beijing"
+            }
+          },
+          "2": {
+            "0": {
+              "v": "Bob"
+            },
+            "1": {
+              "v": 30
+            },
+            "2": {
+              "v": "Shanghai"
+            }
+          }
+        },
+        "tabColor": "",
+        "hidden": 0,
+        "rowCount": 1000,
+        "columnCount": 20,
+        "zoomRatio": 1,
+        "freeze": {
+          "xSplit": 0,
+          "ySplit": 0,
+          "startRow": -1,
+          "startColumn": -1
+        },
+        "scrollTop": 0,
+        "scrollLeft": 0,
+        "defaultColumnWidth": 88,
+        "defaultRowHeight": 24,
+        "mergeData": [],
+        "rowData": {},
+        "columnData": {
+          "0": {
+            "w": 260.60546875
+          }
+        },
+        "showGridlines": 1,
+        "rowHeader": {
+          "width": 46,
+          "hidden": 0
+        },
+        "columnHeader": {
+          "height": 20,
+          "hidden": 0
+        },
+        "rightToLeft": 0
+      }
+    },
+    "resources": []
+  }

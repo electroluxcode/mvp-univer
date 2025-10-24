43de8ed1eac913b6240930dfa4c5495ff322cbec
electroluxcode <3451613934@qq.com>
2025-10-24 18:42:12 +0800

    update home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,11 @@
-import React from 'react'
-
+import React, { useEffect } from 'react'
+import { useRouter } from 'next/navigation'
 function page() {
+  const router = useRouter()
+  // 跳转到 excel 页面
+  useEffect(() => {
+    router.push('/excel')
+  }, [])
   return (
     <div>page</div>
   )
